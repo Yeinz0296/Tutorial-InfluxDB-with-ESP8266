8cfc16d33114ff8021905a04554bfb2ea530aef0
Hazrien Nazman <88524762+Yeinz0296@users.noreply.github.com>
2021-11-19 16:04:45 +0800

    Add files via upload

---
+++ Sample_Coding/Sample_Coding.ino
@@ -0,0 +1,98 @@
+#if defined(ESP32)
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
+#elif defined(ESP8266)
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+#define DEVICE "ESP8266"
+#endif
+
+#include <InfluxDbClient.h>
+#include <SimpleDHT.h>
+int pinDHT11 = D1;
+SimpleDHT11 dht11(pinDHT11);
+
+// WiFi AP SSID
+#define WIFI_SSID "ssid"
+// WiFi password
+#define WIFI_PASSWORD "password"
+// InfluxDB  server url. Don't use localhost, always server name or ip address.
+// E.g. http://192.168.1.48:8086 (In InfluxDB 2 UI -> Load Data -> Client Libraries), 
+#define INFLUXDB_URL "influxdb-url"
+// InfluxDB 2 server or cloud API authentication token (Use: InfluxDB UI -> Load Data -> Tokens -> <select token>)
+#define INFLUXDB_TOKEN "toked-id"
+// InfluxDB 2 organization id (Use: InfluxDB UI -> Settings -> Profile -> <name under tile> )
+#define INFLUXDB_ORG "org"
+// InfluxDB 2 bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
+#define INFLUXDB_BUCKET "bucket"
+// InfluxDB v1 database name 
+//#define INFLUXDB_DB_NAME "database"
+
+// InfluxDB client instance
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+// InfluxDB client instance for InfluxDB 1
+//InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+
+// Data point
+Point sensor("wifi_status");
+
+void setup() {
+  Serial.begin(115200);
+  // Connect WiFi
+  Serial.println("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  sensor.addTag("device", DEVICE);
+  sensor.addTag("SSID", WiFi.SSID());
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+}
+
+void loop() {
+  byte temperature = 0;
+  byte humidity = 0;
+  
+  int err = SimpleDHTErrSuccess;
+  
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
+
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.println(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+
+  sensor.clearFields();
+  sensor.addField("rssi", WiFi.RSSI());
+  sensor.addField("Temperature", (int)temperature);
+  sensor.addField("Humidity", (int)humidity);
+  sensor.addField("ContohString", "Lapar");
+  
+  Serial.println("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor));
+  // If no Wifi signal, try to reconnect it
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+  // Write point
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  //Wait 2s
+  Serial.println("Wait 2s");
+  delay(2000);
+}
